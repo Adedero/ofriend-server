87ceb482eacf5810d48eea2c6b5388df2d8e4a6b
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-22 00:05:23 +0100

    Changed path of secret key

--- src/firebase/firebase-admin.js
+++ src/firebase/firebase-admin.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const admin = require("firebase-admin");
 
-
 admin.initializeApp({
-  credential: admin.credential.cert(path.join(__dirname, 'ofriend-31059-firebase-adminsdk-wi3xy-596140c6fd.json')),
+  credential: admin.credential.cert(process.env.NODE_ENV === 'production' ?
+    '../../ofriend-31059-firebase-adminsdk-wi3xy-596140c6fd.json' :
+    path.join(__dirname, 'ofriend-31059-firebase-adminsdk-wi3xy-596140c6fd.json')),
+
   storageBucket: 'gs://ofriend-31059.appspot.com'
 });
 
