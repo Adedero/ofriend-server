202f8579705a3a02b7a45f3934246d017899bd64
Adedero <118962395+Adedero@users.noreply.github.com>
2024-06-24 22:57:45 +0100

    Update check-auth.js

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -1,5 +1,32 @@
 const passport = require('../config/passport-jwt.config');
 
+const verifyWithoutVerification = async (req, res, next) => {
+  passport.authenticate('jwt', (err, user, info) => {
+    if (err) {
+      return res.status(401).json({
+        success: false,
+        authMessage: true,
+        info: 'An error occurred',
+        message: err
+      }); 
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        authMessage: true,
+        info: 'Unauthorized',
+        message: 'Please, sign in to continue'
+      });
+    }
+
+    req.user = {
+      ...user,
+      id: user._id.toString(), 
+    };
+    next();
+  })(req, res, next);
+}
 const verifyAuth = async (req, res, next) => {
   passport.authenticate('jwt', (err, user, info) => {
     if (err) {
